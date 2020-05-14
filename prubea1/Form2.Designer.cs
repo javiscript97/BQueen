ccfe8bec1a84e4b55be130828a393fd188a6a05f
John Marco <mrbruno2704.mm@gmail.com>
2020-05-14 22:55:14 +0200

    d

--- prubea1/Form2.Designer.cs
+++ prubea1/Form2.Designer.cs
@@ -50,7 +50,6 @@
             this.label11 = new System.Windows.Forms.Label();
             this.button12 = new System.Windows.Forms.Button();
             this.label12 = new System.Windows.Forms.Label();
-            this.button7 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -202,9 +201,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(66, 84);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 51);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(169, 17);
+            this.label1.Size = new System.Drawing.Size(255, 25);
             this.label1.TabIndex = 12;
             this.label1.Text = "La buena hamburguesica";
             // 
@@ -284,21 +284,11 @@
             this.label12.TabIndex = 24;
             this.label12.Text = "La buena hamburguesica";
             // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(399, 12);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(258, 73);
-            this.button7.TabIndex = 1;
-            this.button7.Text = "CAMBIO";
-            this.button7.UseVisualStyleBackColor = true;
-            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1044, 874);
-            this.Controls.Add(this.button7);
             this.Controls.Add(this.tabControl1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form2";
@@ -337,6 +327,5 @@
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Button button12;
         private System.Windows.Forms.Label label12;
-        private System.Windows.Forms.Button button7;
     }
 }
