02223b663f9482c9ac0119f1283fb6301a417fca
Javier Martin Plaza <javimpl97@gmail.com>
2020-05-14 18:18:19 +0200

    CAMBIOS CAMBIOS

--- prubea1/Form2.Designer.cs
+++ prubea1/Form2.Designer.cs
@@ -50,8 +50,7 @@
             this.label11 = new System.Windows.Forms.Label();
             this.button12 = new System.Windows.Forms.Button();
             this.label12 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -285,31 +284,21 @@
             this.label12.TabIndex = 24;
             this.label12.Text = "La buena hamburguesica";
             // 
-            // label7
+            // button7
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(392, 42);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(46, 17);
-            this.label7.TabIndex = 1;
-            this.label7.Text = "HOLA";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(611, 42);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(46, 17);
-            this.label8.TabIndex = 2;
-            this.label8.Text = "HOLA";
+            this.button7.Location = new System.Drawing.Point(399, 12);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(258, 73);
+            this.button7.TabIndex = 1;
+            this.button7.Text = "CAMBIO";
+            this.button7.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1044, 874);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button7);
             this.Controls.Add(this.tabControl1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form2";
@@ -322,7 +311,6 @@
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -349,7 +337,6 @@
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Button button12;
         private System.Windows.Forms.Label label12;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button7;
     }
 }
